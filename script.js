c723bfe0bd2c4918e8066e2e1372c71c560031b7
Zephaniah Ong <zealorez@gmail.com>
2023-01-28 21:46:44 +0800

    Material for day2

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Recap
+Black box 1: Whatever is returned, will appear in the gray box
+Black box 2: input is whatever the user types in the input box
+*/
+
+/* Today's topics
+1. console.log()
+*/
+
+// Group exercise: Return the square of the number given by the user
+var main = function (input) {
+  return "hello " + input;
+};
