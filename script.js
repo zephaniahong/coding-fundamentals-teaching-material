578cbc769fabdb035d681810bc5f7a15fd40d845
Zephaniah Ong <zealorez@gmail.com>
2023-02-02 19:28:04 +0800

    Add topic and grading exercise

--- script.js
+++ script.js
@@ -1,52 +1,17 @@
 /* Today's topics
-1. Functions
-  - Purpose
-    * Reusability
-    * Encapsulation
-  - 2 Steps!!!
-    * Declare
-    * Call
-  - Arguments vs Parameters
-  - Mental Model
-    * Secondary school math functions -> f(x) = x + 5
+1. If statements 
+2. Booleans
+  - or
+  - and
+  - not
 */
 
-// f(x) = 5
-// No parameters
-function giveFive() {
-  return 5;
-}
-
-// f(x) = 3
-function giveThree() {
-  return 3;
-}
-
-// Above functions do not make use of x
-
-// Single parameters
-// f(x) = x
-function giveX(x) {
-  return x;
-}
-
-// f(x) = x + 3
-function addThree(x) {
-  return x + 3;
-}
-
-// Multiple parameters
-// f(x, y, z) = x + y + z
-function sumNums(a, b, c) {
-  return a + b + c;
-}
-
-// practical example
-// f(r) = 3.14 * r ** 2
-function getAreaOfCircle(radius) {
-  return 3.14 * radius ** 2;
-}
-
-var main = function (input) {
-  return "hello " + input;
-};
+// Exercise
+// Grade student's result
+// Extra: Solve without using the 'and' boolean
+// Fail - Less than 50
+// D: 50 - 59
+// C: 60 - 69
+// B: 70 - 79
+// A: 80 - 89
+// A+: 90 - 100
