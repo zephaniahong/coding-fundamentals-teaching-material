4eaded889afb657c53617425cec546896a4ac644
Zephaniah Ong <zealorez@gmail.com>
2023-02-06 13:26:44 +0800

    Add examples

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 /* Today's topics
 1. If statements 
 2. Booleans
+  - not
   - or
   - and
-  - not
 */
 
-function main(input) {
-  // weeks to minutes conversion
-  return `${input} weeks is equivalent to ${Number(input) * 10080} minutes`;
-  // input -> 1 : week
-  // input > 1: weeks
-}
+// Operators
+// === // = = =
+// !== // ! = =
+// >= // > =
+// <= // < =
+// >
+// <
+// or -> ||
+// and -> &&
+
+// Boolean examples
+// if i am above 21, I am allowed to drink
+// if i am not above 18, I cannot drive
+
+// Boolean and
+// if i am above 20 and below 40, I am a youth
+
+// Boolean or
+// if i am above 13 years old or I am at least 160cm, I am allowed to ride the rollercoaster
+
+// Using not
+// !(5 === 10) -> true
+// 5 !== 10 -> true
+// !true -> false
+// !false -> true
 
 // Exercise
 // Grade student's result
-// Extra: Solve without using the 'and' boolean
-// Fail - Less than 50
+// Extra: Solve without using and
+// F - Less than 50
 // D: 50 - 59
 // C: 60 - 69
 // B: 70 - 79
