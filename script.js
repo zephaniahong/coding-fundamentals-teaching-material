34ae0efae35cd4a995458b17c73b83cbe0added9
Zephaniah Ong <zealorez@gmail.com>
2023-02-13 15:02:12 +0800

    add day 6 teaching ideas

--- script.js
+++ script.js
@@ -1,46 +1,5 @@
 /*
-Today's topics:
-1. Input validation
-2. Global state
-  - Scoping
-4. Game modes
+1. HTML, CSS
+2. DOM Manipulation
+3. Arrays and For loops (day 7 material)
 */
-
-// // Input Validation Example
-// function main(input) {
-//   // How do I return "This is a valid input" only if the input is greater than 10? (Otherwise, return "Invalid")
-//   return "This is a valid input";
-// }
-
-// // Global state
-// var chicken = 5;
-
-// function main(input) {
-//   var chicken = 10;
-//   // Quiz: What will be the value of chicken in the console.log?
-//   console.log(chicken);
-//   // Quiz: If chicken is not declared in main, can I still console.log chicken in main? Why or why not?
-// }
-
-// // Global state persists
-// var rice = 0;
-
-// function main(input) {
-//   rice += 1;
-//   return rice;
-// }
-
-// // Game Modes
-// var mode = "player";
-
-// function main(input) {
-//   if (input < 100) {
-//     mode = "computer";
-//   } else {
-//     mode = "player";
-//   }
-//   return `The game mode is ${mode}.`;
-// }
-
-// Exercise
-// Create a game that consists of 2 players. Begin by getting 2 names from the user. Based on the input given by the user (i.e. either of the 2 names) increment the respective player's score
