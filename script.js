eaa6f85c25925ee7a3b43386df970ac3d3503686
Zephaniah Ong <zealorez@gmail.com>
2023-02-20 14:50:03 +0800

    Draft basic version of game logic

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-/* Day 8
-1. Looping over Arrays
-*/
+const boxes = document.getElementsByClassName("box");
 
-// 1. Looping over Arrays
-var rainbow = ["red", "green", "blue"];
-for (var i = 0; i < rainbow.length; i += 1) {
-  console.log(rainbow[i]);
+var mode = "X";
+var placements = [];
+
+for (let i = 0; i < boxes.length; i += 1) {
+  boxes[i].addEventListener("click", () => {
+    if (boxes[i].innerHTML !== "") {
+      console.error("This box is already taken. Try another box!");
+    } else {
+      boxes[i].innerHTML = mode;
+      placements[i] = mode;
+      // check for winning condition
+      if (gameOver()) {
+        console.log("someone won");
+      }
+    }
+    if (mode === "X") {
+      mode = "O";
+    } else {
+      mode = "X";
+    }
+  });
+}
+
+function gameOver() {
+  return (
+    (placements[0] === placements[1] &&
+      placements[1] === placements[2] &&
+      placements[0] !== undefined) ||
+    (placements[3] === placements[4] &&
+      placements[4] === placements[5] &&
+      placements[3] !== undefined) ||
+    (placements[6] === placements[7] &&
+      placements[7] === placements[8] &&
+      placements[6] !== undefined)
+  );
 }
