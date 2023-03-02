4cf87d83b0e6a40198d1de93b9b5f8a98a2c384d
Zephaniah Ong <zealorez@gmail.com>
2023-03-02 21:08:59 +0800

    j

--- __tests__/script.js
+++ __tests__/script.js
@@ -3,6 +3,11 @@
  * 1. Testing
  * 2. Tic Tac Toe
  */
+var add2 = require("../script");
+
+test("2 + 5 is 7", () => {
+  expect(add2(5)).toEqual(7);
+});
 
 function sum(a, b) {
   return a + b;
@@ -16,24 +21,37 @@ function getRandomNumber() {
   return Math.ceil(Math.random() * 6);
 }
 
-test("get random number between 0 and 6", () => {
-  const randomNumber = getRandomNumber();
+test("get random number between 1 and 6", () => {
+  var randomNumber = getRandomNumber();
   expect(randomNumber).toBeGreaterThan(0);
   expect(randomNumber).toBeLessThanOrEqual(6);
 });
 
-test("2 equal arrays", () => {
-  const a = [1, 2, 3];
-  const b = [1, 2, 3];
-  expect(a).toContain(2);
-});
+// test("2 equal arrays", () => {
+//   const a = [1, 2, 3];
+//   const b = [1, 2, 3];
+//   expect(a).toContain(2);
+// });
 
-function getWinner(a, b) {
-  return "player";
+// {value: 5, suit: "hearts"}
+function getWinner(card1, card2) {
+  if (card1.value > card2.value) {
+    return "player";
+  } else {
+    return "computer";
+  }
 }
 
 test("Card with higher value should win", () => {
   const playerCard = { value: 5, suit: "hearts" };
   const computerCard = { value: 12, suit: "clubs" };
-  expect(getWinner(playerCard, computerCard)).toEqual("player");
+  expect(getWinner(playerCard, computerCard)).toEqual("computer");
+});
+
+function square(a) {
+  return a * a;
+}
+
+test("square of 5 is 25", () => {
+  expect(square(5)).toEqual(25);
 });
---
+++ script.js
@@ -0,0 +1,58 @@
+// function test(message, testFunc) {
+//   console.log(message);
+//   testFunc();
+// }
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// // test("sum of 2 and 3 is 5", () => {
+// //   var o = expect(5);
+// //   console.log(o);
+// // });
+
+// function chicken() {
+//   console.log("i am a chicken");
+// }
+// var c = {
+//   value: 5,
+//   toBe: chicken,
+// };
+// c.toBe();
+
+// // function expect(val) {
+// //   return {
+// //     toBe: (expected) => {
+// //       if (expected === val) {
+// //         console.log("passed");
+// //       } else {
+// //         console.log("failed");
+// //       }
+// //     },
+// //   };
+// // }
+// // function test(message, testFunc) {
+// //   console.log(message);
+// //   testFunc();
+// // }
+
+// // function expect(val) {
+// //   return {
+// //     toBe: (expected) => {
+// //       if (expected !== val) {
+// //         console.log(`Expected ${expected}
+// // Received ${val}`);
+// //       } else {
+// //         console.log("✅ Passed");
+// //       }
+// //     },
+// //   };
+// // }
+
+// // function sum(a, b) {
+// //   return a + b;
+// // }
+
+// // test("2 + 3 is 5", () => {
+// //   expect(sum(2, 3)).toBe(7);
+// // });
