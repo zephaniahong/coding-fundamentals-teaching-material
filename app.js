9c5bb67901ecdfb5715ed54e626edd059ccd88d4
Zephaniah Ong <zealorez@gmail.com>
2023-03-25 14:20:55 +0800

    changes made during class

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
+// create any javascript file
+// open terminal: download express
+// run: npm install express --save
+
+// other package managers: yarn, pnpm
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8000; // 3000
+
 const db = require("./db.json");
-console.log(db.ditto);
 
-app.get("/", (req, res) => {
-  res.send({ data: "hello word" });
+app.post("/book/:start/:destination", (req, res) => {
+  // complex logic here
+  // figure out which cab is in vicinity
+  // add a row in the DB
+});
+// endpoint, api route
+app.get("/pokemon/chicken/:pokemonName/:id", (req, res) => {
+  // res.send({ data: "hello word" });
+  // sql
+  const p = req.params;
+  const id = req.params.id;
+  const pokemon = req.params.pokemonName;
+  // console.log(p);
+  // const pokemon = p.pokemonName;
+  // res.send(db[pokemon]);
+  res.send(p);
 });
 
-app.get("/users/:id", (req, res) => {
-  console.log(req);
-  res.send({ data: "users" });
-});
+// app.get("/pokemon/charizard", (req, res) => {
+//   // res.send({ data: "hello word" });
+//   res.send({
+//     abilities: [{ ability: { name: "limber" } }],
+//   });
+// });
+
+// app.get("/users/:id", (req, res) => {
+//   console.log(req);
+//   res.send({ data: "users" });
+// });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- script.js
+++ script.js
@@ -14,18 +14,45 @@
 
 // Request & Response (Client & Server)
 // I am the client, making a REQUEST to the google URL. Some server will respond to this request by returning me some data.
+// console.log("before fetch");
 // fetch("https://www.google.com").then((res) => {
 //   console.log(res);
 // });
 
+// console.log("i am the first");
 // Asynchronous vs Synchronous
 // console.log("I am after fetch");
 
-// APIs
+// APIs (Application Programming Interface)
+// test("message here", () => {
+//   // testing logic
+//   expect(3, 3);
+// });
+
 // Calling an API: I make a request through a RESTful API (which looks like a URL). The server hosting the API will then respond to my request
-// fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-//   .then((res) => res.json())
+// JSON - javascript object notation
+// GET, PUT, POST, PATCH
+// fetch("https://pokeapi.co/api/v2/move/132")
+//   .then((response) => response.json())
 //   .then((data) => console.log(data));
+
+fetch("http://localhost:8000/pokemon/ditto")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+var button = document.createElement("btn");
+var a = document.createElement("div");
+button.addEventListener("click", () => {
+  fetch("localhost:8000/book/toapayoh/changiairport", { method: "POST" })
+    .then((response) => response.json())
+    .then((data) => {
+      var pokemonName = data.name;
+      a.innerText = pokemonName;
+      document.body.appendChild(a);
+    });
+});
+// some super complex and slow algorithm
+// console.log("after fetch");
 
 // fetch("http://localhost:8000/")
 //   .then((data) => data.json())
