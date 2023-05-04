8fa316159969c4568690e0d5ac44059cfa72fc93
Zephaniah Ong <zealorez@gmail.com>
2023-05-04 15:33:23 +0800

    day 1

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-var main = function (input) {
-  return "hello " + input;
-};
+/* Day 1
+1. Admin
+- Course pace
+- Class pace
+- VSCode setup
+
+2. Operations
+- addition, subtraction, multiplication, division, power, modulus
+
+// types
+// number, strings, arrays, objects
+
+3. Variables
+- What are they?
+- 2 step process
+  - declare
+  - assign
+- Mental model
+*/
+
+// Quiz
+var chicken = 5;
+var rice = 3;
+var dish = chicken + rice;
+chicken = 10;
+dish; // What is the value of dish here?
